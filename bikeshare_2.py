23395884ebe415c1090219bc0f0f5c6c3e027db2
Abdulhay <abdomulla@hotmail.com>
2021-07-07 18:45:52 +0300

    Remove spaces in new york city key

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -11,7 +11,7 @@ import pandas as pd
 import numpy as np
 
 CITY_DATA = { 'chicago': 'chicago.csv',
-              'new york city': 'new_york_city.csv',
+              'new_york_city': 'new_york_city.csv',
               'washington': 'washington.csv' }
 
 
